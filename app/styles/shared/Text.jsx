da95b739ed884dec746b2896d0a22ec9d52d9b41
E-Zbox <zboxstudios97@gmail.com>
2023-07-12 09:48:54 +0100

    added H3 reusable styling

--- app/styles/shared/Text.jsx
+++ app/styles/shared/Text.jsx
@@ -4,6 +4,12 @@ export const H1 = ({ children, className }) => (
     </h1>
 );
 
+export const H3 = ({ children, className }) => (
+    <h3 className={`text-xl capitalize font-sans pl-3 mb-2 ${className}`}>
+        {children}
+    </h3>
+);
+
 export const H4 = ({ children, className }) => (
-    <h4 className={`text-2xl capitalize font-mono ${className}`}>{children}</h4>
+    <h4 className={`text-xl capitalize font-mono ${className}`}>{children}</h4>
 );
