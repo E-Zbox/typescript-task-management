8ce1de8b8fd750b47eeeabf55c0300f43cf0bb18
E-Zbox <zboxstudios97@gmail.com>
2023-07-12 09:42:05 +0100

    implemented single source of truth for ModalStore

--- app/store/index.tsx
+++ app/store/index.tsx
@@ -1,13 +1,20 @@
 // this is the one point of every store initialization
 import * as form from "./form";
+import * as modal from "./modal";
 import * as task from "./task";
 
-const FormStore = new form.FormStore()
+const FormStore = new form.FormStore();
+
+const ModalStore = new modal.ModalStore();
 
 const TaskStore = new task.TaskStore();
 
 const _ = {
-    task, form, FormStore, TaskStore, 
-}
+    task,
+    form,
+    FormStore,
+    ModalStore,
+    TaskStore,
+};
 
-export default _
+export default _;
