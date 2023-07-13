b888aa1757939f492faf2e9178faf05d4f58bad2
E-Zbox <zboxstudios97@gmail.com>
2023-07-13 17:23:09 +0100

    created reusable text input style component

--- app/styles/shared/Form.tsx
+++ app/styles/shared/Form.tsx
@@ -1,4 +1,11 @@
-import { ButtonHTMLAttributes, FC, OptionHTMLAttributes, ReactNode, SelectHTMLAttributes } from "react";
+import {
+    ButtonHTMLAttributes,
+    FC,
+    InputHTMLAttributes,
+    OptionHTMLAttributes,
+    ReactNode,
+    SelectHTMLAttributes,
+} from "react";
 
 interface ButtonPropTypes extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
@@ -12,7 +19,9 @@ interface SelectPropTypes extends SelectHTMLAttributes<HTMLSelectElement> {
     children: ReactNode;
 }
 
-export const Button: FC<ButtonPropTypes> = ({ children, ...props}) => (
+interface TextInputPropTypes extends InputHTMLAttributes<HTMLInputElement> {}
+
+export const Button: FC<ButtonPropTypes> = ({ children, ...props }) => (
     <button
         className={`px-5 py-3 font-mono bg-blue-600 rounded-full opacity-50 hover:opacity-100`}
         {...props}
@@ -21,11 +30,19 @@ export const Button: FC<ButtonPropTypes> = ({ children, ...props}) => (
     </button>
 );
 
-export const Option: FC<OptionPropTypes> = ({children, ...props})=> (
-    <option className="py-2 px-2" {...props}>{children}</option>
-)
+export const Option: FC<OptionPropTypes> = ({ children, ...props }) => (
+    <option className="py-2 px-2" {...props}>
+        {children}
+    </option>
+);
 
-export const Select: FC<SelectPropTypes> = ({ children, className, name, id, placeholder }) => (
+export const Select: FC<SelectPropTypes> = ({
+    children,
+    className,
+    name,
+    id,
+    placeholder,
+}) => (
     <select
         className={`text-slate-950 py-3 px-2 rounded ${className}`}
         name={name}
@@ -34,4 +51,11 @@ export const Select: FC<SelectPropTypes> = ({ children, className, name, id, pla
     >
         {children}
     </select>
+);
+
+export const TextInput: FC<TextInputPropTypes> = (props) => (
+    <input
+        className={"w-3/5 h-10 px-2 rounded text-slate-950 ml-3"}
+        {...props}
+    />
 );
