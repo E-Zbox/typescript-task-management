21ec2685d52346a606a6a3830f448706a0aed7f5
E-Zbox <zboxstudios97@gmail.com>
2023-07-13 17:21:41 +0100

    added logic to delete task when trash button gets clicked

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -1,6 +1,9 @@
 "use client";
+import Link from "next/link";
 import React from "react";
 import { AiFillDelete } from "react-icons/ai";
+// store
+import store from "../store";
 // styles
 import { Button } from "../styles/shared/Form";
 import { DivContainer, MainFlexContainer } from "../styles/shared/Container";
@@ -11,18 +14,25 @@ interface TaskPropTypes {
     title: string;
 }
 export const Task = ({ _id, title }: TaskPropTypes) => {
+    const { deleteTask } = store.TaskStore;
+
     return (
         <MainFlexContainer
             className={
-                "bg-slate-700 my-1 py-3 px-4 rounded opacity-80 z-0 hover:opacity-100 hover:translate-x-0.5"
+                "bg-slate-700 my-1 py-3 px-4 rounded cursor-pointer opacity-80 z-0 hover:opacity-100 hover:translate-x-0.5"
             }
         >
-            <DivContainer className={"w-full"}>
-                <H4 className={""}>{title}</H4>
-            </DivContainer>
+            <Link className={"w-full"} key={_id} href={`/task/${_id}`}>
+                <DivContainer className={"w-full"}>
+                    <H4 className={""}>{title}</H4>
+                </DivContainer>
+            </Link>
             <DivContainer className={"w-fit"}>
-                <Button className={"bg-transparent"}>
-                    <AiFillDelete size={24} color={"#ae3423"} />
+                <Button
+                    className={"bg-transparent"}
+                    onClick={() => deleteTask(_id)}
+                >
+                    <AiFillDelete color={"#ae3423"} size={24} />
                 </Button>
             </DivContainer>
         </MainFlexContainer>
