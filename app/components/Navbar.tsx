179733594b4a4edbd16dea92791ea7727183f344
E-Zbox <zboxstudios97@gmail.com>
2023-07-13 17:19:45 +0100

    included logic to display '+ Task' button only in home page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 // store
 import store from "../store";
@@ -7,19 +9,24 @@ import { Button } from "../styles/shared/Form";
 import { H1 } from "../styles/shared/Text";
 
 const Navbar = () => {
+    const pathname = usePathname();
     const { updateState } = store.ModalStore;
 
     const handleClick = () => updateState({ show: true });
     return (
         <MainFlexContainer
-            className={"border-green-300 px-10 shadow shadow-gray-300/50"}
+            className={"px-10 bg-slate-900 shadow shadow-gray-800/50"}
         >
             <div className="h-fit">
-                <H1 className="py-5 px-2.5">Taskify</H1>
+                <Link href="/">
+                    <H1 className="py-5 px-2.5">Taskify</H1>
+                </Link>
             </div>
-            <div className="h-fit">
-                <Button onClick={handleClick}>+ Task</Button>
-            </div>
+            {pathname.split("/").length === 2 && (
+                <div className="h-fit">
+                    <Button onClick={handleClick}>+ Task</Button>
+                </div>
+            )}
         </MainFlexContainer>
     );
 };
